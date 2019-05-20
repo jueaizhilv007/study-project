ed92401a3ca4965ff703c2c90cdfbeae0657ab8d
rocky <jueaizhilv@126.com>
2019-05-20 20:57:12 +0800

    Initial commit

---
+++ src/test/java/com/study/studyproject/StudyProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.studyproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
